e807d0bca71bfd291af81821afefc44788529a45
hongbae <hongpei17@gmail.com>
2026-06-04 09:59:27 +0300

    Initialize TypeScript project structure

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+import prettier from "eslint-config-prettier";
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint,
+    },
+    rules: {
+      ...tseslint.configs["recommended"].rules,
+      ...tseslint.configs["recommended-requiring-type-checking"].rules,
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-function-return-type": "error",
+    },
+  },
+  prettier,
+  {
+    ignores: ["dist/**", "node_modules/**"],
+  },
+];
