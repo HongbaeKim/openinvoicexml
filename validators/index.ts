78622f43d974d84c3d70bb7c1e1800d9b633f16d
hongbae <hongpei17@gmail.com>
2026-07-06 22:45:24 +0300

    fix: add 2 more export from kosit.js

--- validators/index.ts
+++ validators/index.ts
@@ -5,3 +5,6 @@
 
 export { validateBusinessRules } from "./business-rules.js";
 export type { ValidationIssue } from "./business-rules.js";
+
+export { runKosit } from "./kosit.js";
+export type { KositIssue, KositResult, KositOptions } from "./kosit.js";
